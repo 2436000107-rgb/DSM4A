1cb886162bde1f53b7af0e2f1a8b5118973de744
Dominick-Hms <2436000692@utna.edu.mx>
2025-11-26 15:13:53 -0600

    Holas

--- Hola.js
+++ Hola.js
@@ -8,3 +8,13 @@ Object.defineProperty(DOMRectList.prototype, 'length', {
         return Object.keys(this).length;
     }
 });
+Object.setPrototypeOf(DOMRectList.prototype, Object.prototype);
+export { DOMRectList };
+function DOMRectList() {
+    if (!(this instanceof DOMRectList)) {
+        throw new TypeError("DOMRectList constructor cannot be called as a function.");
+    }
+    for (var i = 0; i < arguments.length; i++) {
+        this[i] = arguments[i];
+    }           
+}
